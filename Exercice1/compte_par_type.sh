ffc7c260292616b0e7e0b1c1e2fb54347cb860ae
Seif <seifmebarki2509@gmail.com>
2025-10-13 19:32:04 +0200

    Mise à jour du script compte par type.sh aprés modification

--- Exercice1/compte_par_type.sh
+++ Exercice1/compte_par_type.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <année> <entité>"
+    exit 1
+fi
 year=$1
 entity=$2
+
+if [[ ! "$year" =~ ^(2016|2017|2018)$ ]]; then
+    echo "Erreur : année invalide. Choisir 2016, 2017 ou 2018."
+    exit 1
+fi
+
 grep -h "$entity" ann/$year/* | wc -l
