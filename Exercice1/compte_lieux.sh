6308e4c9e9e17709e8cae92c330a78d1c28e3ace
Seif <seifmebarki2509@gmail.com>
2025-10-13 19:55:41 +0200

    Mise à jour du script aprés modification

--- Exercice1/compte_lieux.sh
+++ Exercice1/compte_lieux.sh
@@ -4,6 +4,24 @@ year=$1
 month=$2
 top_n=$3
 
+if [[ ! "$year" =~ ^(2016|2017|2018|\*)$ ]]; then
+    echo "Erreur : année invalide. Choisir 2016, 2017, 2018 ou *."
+    exit 1
+fi
+
+
+if [[ ! "$month" =~ ^([0-9]{2}|\*)$ ]]; then
+    echo "Erreur : mois invalide. Format 01..12 ou *."
+    exit 1
+fi
+
+
+if ! [[ "$top_n" =~ ^[0-9]+$ ]]; then
+    echo "Erreur : nombre invalide. Doit être un entier."
+    exit 1
+fi
+
+
 if [ "$year" = "*" ]; then
     files="ann/*/*"
 else
